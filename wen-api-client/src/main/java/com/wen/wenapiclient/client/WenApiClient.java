0c4bf38538385eebc5fe299ad7d7cff7888c3057
CCCshengjiang <123shengjiang@gmail.com>
2024-03-14 23:05:57 +0800

    💥 feat(客户端): 封装 API 调用方法

--- wen-api-client/src/main/java/com/wen/wenapiclient/client/WenApiClient.java
+++ wen-api-client/src/main/java/com/wen/wenapiclient/client/WenApiClient.java
@@ -5,8 +5,11 @@ import cn.hutool.http.HttpRequest;
 import cn.hutool.http.HttpResponse;
 import cn.hutool.http.HttpUtil;
 import cn.hutool.json.JSONUtil;
-import com.wen.wenapiclient.model.UserInClient;
 import com.wen.wenapiclient.util.SignUtil;
+import com.wen.wenapicommon.common.BaseCode;
+import com.wen.wenapicommon.exception.BusinessException;
+import com.wen.wenapicommon.model.domain.User;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -16,10 +19,13 @@ import java.util.Map;
  *
  * @author wen
  */
+@Slf4j
 public class WenApiClient {
 
-    private String accessKey;
-    private String secretKey;
+    private final String accessKey;
+    private final String secretKey;
+
+    private static final String GATEWAY_HOST = "http://localhost:8090";
 
     public WenApiClient(String accessKey, String secretKey) {
         this.accessKey = accessKey;
@@ -30,7 +36,7 @@ public class WenApiClient {
         //可以单独传入http参数，这样参数会自动做URL编码，拼接在URL中
         HashMap<String, Object> paramMap = new HashMap<>();
         paramMap.put("name", name);
-        String result= HttpUtil.get("http://localhost:8123/api/name/", paramMap);
+        String result = HttpUtil.get("/api/name/", paramMap);
         System.out.println("客户端：" + result);
         return result;
     }
@@ -38,20 +44,27 @@ public class WenApiClient {
     public String getNameByPost(String name) {
         HashMap<String, Object> paramMap = new HashMap<>();
         paramMap.put("name", name);
-        String result= HttpUtil.post("http://localhost:8123/api/name/", paramMap);
+        String result = HttpUtil.post(GATEWAY_HOST + "/api/name/", paramMap);
         System.out.println("客户端：" + result);
         return result;
     }
 
-    public String getUsernameByPost(UserInClient user) {
+    public String getUsernameByPost(User user) {
         String json = JSONUtil.toJsonStr(user);
-        HttpResponse httpResponse = HttpRequest.post("http://localhost:8123/api/name/user")
+        HttpResponse httpResponse = HttpRequest.post(GATEWAY_HOST + "/api/name/user")
                 .addHeaders(getHeaderMap(json))
                 .body(json)
                 .execute();
-        System.out.println("客户端响应：" + httpResponse);
+        if (httpResponse.getStatus() == 500) {
+            throw new BusinessException(BaseCode.RATE_LIMIT_EXCEEDED, "接口调用次数已用完，请联系管理员！");
+        }
+        if (httpResponse.getStatus() == 403) {
+            throw new BusinessException(BaseCode.ACCESS_DENIED, "用户权限不足！");
+        }
+        log.info("当前调用接口：getUsernameByPost");
+        log.info("客户端响应：" + httpResponse);
         String res = httpResponse.body();
-        System.out.println("客户端返回：" + res);
+        log.info("客户端返回：" + res);
         return res;
     }
 

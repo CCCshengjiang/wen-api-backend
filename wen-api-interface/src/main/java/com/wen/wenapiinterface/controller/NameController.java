0ade1e4a6cb051c9c41823ba31f5dac188d43cd6
CCCshengjiang <123shengjiang@gmail.com>
2024-03-18 18:55:27 +0800

    💥 feat(接口平台): 管理四个接口

--- wen-api-interface/src/main/java/com/wen/wenapiinterface/controller/NameController.java
+++ wen-api-interface/src/main/java/com/wen/wenapiinterface/controller/NameController.java
@@ -1,8 +1,9 @@
 package com.wen.wenapiinterface.controller;
 
-import com.wen.wenapiclient.model.UserInClient;
-import com.wen.wenapiclient.util.SignUtil;
-import jakarta.servlet.http.HttpServletRequest;
+import cn.hutool.json.JSON;
+import com.wen.wenapicommon.model.domain.User;
+import com.wen.wenapiinterface.util.RequestUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -11,32 +12,70 @@ import org.springframework.web.bind.annotation.*;
  * @author wen
  */
 @RestController
-@RequestMapping("/name")
+@RequestMapping("/")
 public class NameController {
 
-    @GetMapping("/")
-    public String getNameByGet(@RequestParam String name) {
-        return "GET 名字是：" + name;
-    }
-
-    @PostMapping("/")
-    public String getNameByPost(@RequestParam String name) {
-        return "POST 名字是：" + name;
-    }
-
-    @PostMapping("/user")
-    public String getUsernameByPost(@RequestBody UserInClient user, HttpServletRequest request) {
-        String accessKey = request.getHeader("accessKey");
-        if (!"wen-api".equals(accessKey)) {
-            throw new RuntimeException("无权限调用改接口");
-        }
-        // 实际情况是应该去数据库中查找 secretKey
-        String sign = SignUtil.getSign(request.getHeader("body"), "admin");
-        String serveSign = request.getHeader("sign");
-        if (!sign.equals(serveSign)) {
-            throw new RuntimeException("无权限调用改接口");
-        }
+    /**
+     * 得到当前用户的用户名
+     *
+     * @param user 当前用户
+     * @return 当前用户的用户名
+     */
+    @PostMapping("/name")
+    public String getUsernameByPost(@RequestBody User user) {
         return "POST(JSON) 名字是：" + user.getUsername();
     }
+
+    /**
+     * 毒鸡汤
+     *
+     * @return 得到一句随机毒鸡汤
+     */
+    @GetMapping("/poison-chicken-soup")
+    public JSON getPoisonChickenSoup() {
+        return RequestUtils.getJson("https://api.btstu.cn/yan/api.php?charset=utf-8&encode=json");
+    }
+
+    /**
+     * 情话
+     *
+     * @return 得到一句随机情话
+     */
+    @GetMapping("/love-word")
+    public JSON getLoveWords() {
+        return RequestUtils.getJson("https://api.vvhan.com/api/love");
+    }
+
+    /**
+     * 天气
+     *
+     * @return 得到天气
+     */
+    @PostMapping("/weather")
+    public JSON getWeather(@RequestBody(required = false) String city) {
+        String url = "https://api.vvhan.com/api/weather";
+        if (StringUtils.isNotBlank(city)) {
+            url = url + "?city=" + city;
+        }
+        return RequestUtils.getJson(url);
+    }
+
+    /**
+     * 翻译
+     *
+     * @param words 前端传递的句子
+     * @return 翻译后的 JSON 格式句子
+     */
+    @PostMapping("/translation")
+    public JSON getLoveWords(@RequestBody String words) {
+        String url = "https://api.vvhan.com/api/fy";
+        if (StringUtils.isNotBlank(words)) {
+            url = url + "?text=" + words;
+        }
+        return RequestUtils.getJson(url);
+    }
+
+
+
 
 }
--- wen-api-interface/src/test/java/com/wen/wenapiinterface/WenApiInterfaceApplicationTests.java
+++ wen-api-interface/src/test/java/com/wen/wenapiinterface/WenApiInterfaceApplicationTests.java
@@ -1,26 +1,16 @@
 package com.wen.wenapiinterface;
 
-import com.wen.wenapiclient.WenApiClientConfig;
 import com.wen.wenapiclient.client.WenApiClient;
-import com.wen.wenapiclient.model.UserInClient;
+import com.wen.wenapicommon.model.domain.User;
 import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 
 @SpringBootTest
-/*@Import(WenApiClientConfig.class)  // 导入配置类*/
 class WenApiInterfaceApplicationTests {
-
-    @Resource
-    private WenApiClient wenApiClient;
 
     @Test
     void testUserPost() {
-        UserInClient user = new UserInClient();
-        user.setUsername("wen");
-        String usernameByPost = wenApiClient.getUsernameByPost(user);
-        System.out.println(usernameByPost);
     }
 
 }
